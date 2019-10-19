b934a1827357c618a9ce6920cd9c639378cdce09
Hugo Woodiwiss <hj.woodiwiss@gmail.com>
2019-10-19 22:27:01 +0100

    "Liberated" my own bitchin' window class

--- GameboyEmu/Window.h
+++ GameboyEmu/Window.h
@@ -1,16 +1,53 @@
 #pragma once
-
-#include <Windows.h>
 #define WIN32_LEAN_AND_MEAN
+#include <Windows.h>
+#include <vector>
+#include <thread>
+#include <mutex>
+
+typedef void (*WndEvent)(WPARAM, LPARAM);
 
 class Window
 {
 public:
-	Window();
-	HWND GetHwnd();
+
+	//If runThreaded is false, the message loop should be created and handled in the calling thread.
+	Window(HINSTANCE appInstance, bool runThreaded);
+	~Window();
+
+	HWND GetHandle();
+
+	void Add_OnLoad(WndEvent handler);
+	void Remove_OnLoad(WndEvent handler);
+
+	void Add_OnResize(WndEvent handler);
+	void Remove_OnResize(WndEvent handler);
+
+	void Add_OnExit(WndEvent handler);
+	void Remove_OnExit(WndEvent handler);
+
+	bool is_running();
 
 private:
-	bool initialised;
-	HWND windowHandle;
+	void ThreadProc(HINSTANCE);
+	void MessageLoop();
+	void set_running(bool value);
+
+	LRESULT WndProc(HWND windowHandle, UINT message, WPARAM wParam, LPARAM lParam);
+
+	//Static window proc used to direct messages and params to the specific windows own Window proc
+	static LRESULT GlobalMapWndProc(HWND windowHandle, UINT message, WPARAM wParam, LPARAM lParam);
+
+	//Events
+	std::vector<WndEvent> OnLoad;
+	std::vector<WndEvent> OnResize;
+	std::vector<WndEvent> OnExit;
+
+	//Internal usage
+	ATOM m_pWndClass;
+	HWND m_wndHandle;
+	std::thread* m_messageLoop;
+	bool m_running;
+	std::mutex m_lockRunning;
 
 };
