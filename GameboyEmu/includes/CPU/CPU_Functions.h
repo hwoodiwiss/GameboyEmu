1eb60b28f49f7cb43da560f4ed9c6dda9df38704
Hugo Woodiwiss <hj.woodiwiss@gmail.com>
2021-01-14 13:50:00 +0000

    Fixed implewmentations in functions file

--- GameboyEmu/includes/CPU/CPU_Functions.h
+++ GameboyEmu/includes/CPU/CPU_Functions.h
@@ -1,31 +1,50 @@
 ﻿#include <Shared.h>
 #include <MMU/MMU.h>
 
-BYTE f_Zero = 0x80;
-BYTE f_Subtract = 0x40;
-BYTE f_HalfCarry = 0x20;
-BYTE f_Carry = 0x10;
+//Flag definitions for the F register. To add a flag, OR it in.
+extern BYTE f_Zero;
+extern BYTE f_Subtract;
+extern BYTE f_HalfCarry;
+extern BYTE f_Carry;
 
-inline void LD(BYTE& _register, BYTE& operand)
+inline void stackPush(MMU* mmu, WORD& stackPointer, WORD val)
 {
-	_register = operand;
+	mmu->WriteWord(stackPointer, val);
+	stackPointer += 2;
 }
 
-inline void LD(WORD& _reg16, WORD& operand)
+inline WORD stackPop(MMU* mmu, WORD& stackPointer)
 {
-	_reg16 = operand;
+	WORD val = mmu->ReadWord(stackPointer);
+	stackPointer -= 2;
+	return val;
 }
 
-inline void LD(MMU&& mmu, WORD& _address, BYTE& operand)
+inline void LD(BYTE& registerRef, BYTE operand)
+{
+	registerRef = operand;
+}
+
+inline void LD(WORD& wideRegRef, BYTE operand)
+{
+	wideRegRef = operand;
+}
+
+inline void LD(WORD& wideRegRef, WORD operand)
+{
+	wideRegRef = operand;
+}
+
+inline void LD(MMU&& mmu, WORD& _address, BYTE operand)
 {
 	mmu.WriteByte(_address, operand);
 }
 
-inline void ADD(BYTE& _register, BYTE& flagRegister, BYTE& operand)
+inline void ADD(BYTE& registerRef, BYTE& flagRegister, BYTE operand)
 {
-	WORD res = _register + operand;
+	WORD res = registerRef + operand;
 
-	if ((((operand >> 8) & 0xF) + ((_register >> 8) & 0xF)) & 0x10)
+	if ((((operand >> 8) & 0xF) + ((registerRef >> 8) & 0xF)) & 0x10)
 	{
 		flagRegister |= f_HalfCarry;
 	}
@@ -38,16 +57,16 @@ inline void ADD(BYTE& _register, BYTE& flagRegister, BYTE& operand)
 		flagRegister |= f_Zero;
 	}
 	flagRegister &= ~f_Subtract;
-	_register = (BYTE)res;
+	registerRef = (BYTE)res;
 }
 
-inline void ADD(WORD& _register, BYTE& flagRegister, WORD operand)
+inline void ADD(WORD& registerRef, BYTE& flagRegister, WORD operand)
 {
-	WORD target = _register;
+	WORD target = registerRef;
 
 	WORD result = operand + target;
 
-	_register = result;
+	registerRef = result;
 
 	if ((((operand >> 8) & 0xF) + ((target >> 8) & 0xF)) & 0x10)
 	{
@@ -60,36 +79,36 @@ inline void ADD(WORD& _register, BYTE& flagRegister, WORD operand)
 	flagRegister &= ~f_Subtract;
 }
 
-inline void ADC(BYTE& _register, BYTE& flagRegister, BYTE& operand)
+inline void ADC(BYTE& registerRef, BYTE& flagRegister, BYTE operand)
 {
-	if ((_register & 0x8) == 0x8)
+	if ((registerRef & 0x8) == 0x8)
 	{
 		flagRegister |= f_HalfCarry;
 	}
-	_register = _register + operand;
-	if (_register == 0)
+	registerRef = registerRef + operand;
+	if (registerRef == 0)
 	{
 		flagRegister |= f_Zero;
 	}
 }
 
-inline void ADC(BYTE& _register, WORD operand)
+inline void ADC(BYTE& registerRef, WORD operand)
 {
 }
 
-inline void BIT(BYTE& _register, BYTE nBit)
+inline void BIT(BYTE& registerRef, BYTE& flagRegister, BYTE nBit)
 {
 	BYTE nMask = ((BYTE)1 << (BYTE)nBit);
-	if ((_register & nMask) == nMask)
+	if ((registerRef & nMask) == nMask)
 	{
-		F &= ~nMask;
+		registerRef &= ~nMask;
 	}
 	else
 	{
-		F |= nMask;
+		flagRegister |= nMask;
 	}
-	F &= ~f_Subtract;
-	F |= f_HalfCarry;
+	flagRegister &= ~f_Subtract;
+	flagRegister |= f_HalfCarry;
 }
 
 inline void SUB(BYTE operand)
@@ -100,58 +119,58 @@ inline void SUB(WORD operand)
 {
 }
 
-inline void SBC(BYTE& _register, BYTE operand)
+inline void SBC(BYTE& registerRef, BYTE& flagRegister, BYTE operand)
 {
-	if ((_register & 0x10) == 0x10)
+	if ((registerRef & 0x10) == 0x10)
 	{
-		F |= f_HalfCarry;
+		flagRegister |= f_HalfCarry;
 	}
-	_register -= operand;
-	if (_register == 0)
+	registerRef -= operand;
+	if (registerRef == 0)
 	{
-		F |= f_Zero;
+		flagRegister |= f_Zero;
 	}
-	F |= f_Subtract;
+	flagRegister |= f_Subtract;
 }
 
-inline void SBC(BYTE& _register, WORD operand)
+inline void SBC(BYTE& registerRef, WORD operand)
 {
 }
 
-inline void INC(BYTE& _register)
+inline void INC(BYTE& registerRef, BYTE& flagRegister)
 {
-	if ((_register & 0xF) == 0xF)
+	if ((registerRef & 0xF) == 0xF)
 	{
-		F |= f_HalfCarry;
+		flagRegister |= f_HalfCarry;
 	}
-	_register++;
-	if (_register == 0)
+	registerRef++;
+	if (registerRef == 0)
 	{
-		F |= f_Zero;
+		flagRegister |= f_Zero;
 	}
-	F &= ~f_Subtract;
+	flagRegister &= ~f_Subtract;
 }
 
 //Increment a 16-bit register value.
-//TODO: Add soul-crushing flag manipulation
-inline void INC(WORD& reg16)
+//TODO: Add flag setting
+inline void INC(WORD& reg16, BYTE& flagRegister)
 {
 	reg16++;
 }
 
 //8-bit decrement. Extra complex, as we have to decrement the value, and set all the required flags.
-inline void DEC(BYTE& _register)
+inline void DEC(BYTE& registerRef, BYTE& flagRegister)
 {
-	if ((_register & 0x10) == 0x10)
+	if ((registerRef & 0x10) == 0x10)
 	{
-		F |= f_HalfCarry;
+		flagRegister |= f_HalfCarry;
 	}
-	_register--;
-	if (_register == 0)
+	registerRef--;
+	if (registerRef == 0)
 	{
-		F |= f_Zero;
+		flagRegister |= f_Zero;
 	}
-	F |= f_Subtract;
+	flagRegister |= f_Subtract;
 }
 
 inline void DEC(WORD& reg16)
@@ -159,70 +178,70 @@ inline void DEC(WORD& reg16)
 	reg16--;
 }
 
-inline void JR(bool condition, SBYTE offset)
+inline void JR(unsigned long long& clock, WORD& programCounter, bool condition, SBYTE offset)
 {
 	if (condition)
 	{
 		clock += 4; //Have to add to clock, as branching takes longer than not branching
-		_PC += offset;
+		programCounter += offset;
 	}
 }
 
-inline void XOR(BYTE operand)
+inline void XOR(BYTE& registerRef, BYTE& flagRegister, BYTE operand)
 {
-	A ^= operand;
-	if (A == 0)
+	registerRef ^= operand;
+	if (registerRef == 0)
 	{
-		F |= f_Zero;
+		flagRegister |= f_Zero;
 	}
-	F &= ~f_Carry;
-	F &= ~f_HalfCarry;
-	F &= ~f_Subtract;
+	flagRegister &= ~f_Carry;
+	flagRegister &= ~f_HalfCarry;
+	flagRegister &= ~f_Subtract;
 }
 
 //Unconditional return
-inline void RET()
+inline void RET(MMU* mmu, WORD& stackPointer, WORD& programCounter)
 {
-	_PC = stackPop();
+	programCounter = stackPop(mmu, stackPointer);
 }
 
 //Conditional return
-inline void RET(bool condition)
+inline void RET(MMU* mmu, WORD& stackPointer, WORD& programCounter, unsigned long long& clock, bool condition)
 {
 	if (condition)
 	{
 		clock += 12; //Have to add to clock, as branching takes longer than not branching
-		_PC = stackPop();
+		programCounter = stackPop(mmu, stackPointer);
 	}
 }
 
-inline void PUSH(WORD address)
+inline void PUSH(MMU* mmu, WORD& stackPointer, WORD address)
 {
-	stackPush(address);
+	stackPush(mmu, stackPointer, address);
 }
 
-inline void POP(WORD& reg16)
+inline void POP(MMU* mmu, WORD& stackPointer, WORD& reg16)
 {
-	WORD var = _mmu->ReadWord(_SP);
-	DecSP();
-	reg16 = var;
+	reg16 = stackPop(mmu, stackPointer);
 }
 
-inline void CALL(WORD addr)
+inline void CALL(MMU* mmu, WORD& stackPointer, WORD& programCounter, bool& noInc, WORD addr)
 {
-	stackPush(_PC + 1);
+	stackPush(mmu, stackPointer, programCounter + 1);
 	noInc = true;
-	_PC = addr;
+	programCounter = addr;
 }
 
 
 //Conditional call
-inline void CALL(bool condition, WORD addr)
+inline void CALL(MMU* mmu, WORD& stackPointer, WORD& programCounter, unsigned long long& clock, bool& noInc, bool condition, WORD addr)
 {
 	if (condition)
 	{
 		clock += 12; //Have to add to clock, as branching takes longer than not branching
-		stackPush(_PC + 1);
+		stackPush(mmu, stackPointer, programCounter + 1);
 		noInc = true;
-		_PC = addr;
+		programCounter = addr;
 	}
+}
+
---
+++ GameboyEmu/src/CPU/CPU_Functions.cpp
@@ -0,0 +1,6 @@
+#include <CPU/CPU_Functions.h>
+
+BYTE f_Zero = 0x80;
+BYTE f_Subtract = 0x40;
+BYTE f_HalfCarry = 0x20;
+BYTE f_Carry = 0x10;
